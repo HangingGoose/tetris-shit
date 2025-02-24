d872d7845c2a464bb42a93b952c98b250cd71bec
HangingGoose <matti.lehtinen@telenet.be>
2025-02-24 13:40:50 +0100

    pluh

--- tetris-shit/src/main/java/kkr/paljas/tetris/Main.java
+++ tetris-shit/src/main/java/kkr/paljas/tetris/Main.java
@@ -1,7 +1,18 @@
 package kkr.paljas.tetris;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Tetris");
+        stage.setResizable(false);
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
     }
 }
